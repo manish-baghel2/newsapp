d9f3722134378c62bfbfe2ef506d7a69c468ae47
Manish Baghel <manish_11914130@nitkkr.ac.in>
2022-09-22 13:01:10 +0530

    added previous and next button to access more page

--- src/NewsItem.js
+++ src/NewsItem.js
@@ -5,11 +5,11 @@ export class NewsItem extends Component {
     let {title, description, imageUrl, newsUrl} = this.props;
     return (
       <div className="card my-3" style={{width: "18rem"}}>
-        <img className="card-img-top" src={imageUrl} alt="..."/>
+        <img className="card-img-top" src={imageUrl?imageUrl:"https://images.moneycontrol.com/static-mcnews/2021/09/dollar-758x435.jpg"} alt="..."/>
           <div className="card-body">
             <h5 className="card-title">{title}...</h5>
             <p className="card-text">{description}...</p>
-            <a href={newsUrl} className="btn btn-sm btn-primary" target="_blank" rel="noopener noreferrer">Read More</a>
+            <a href={newsUrl} className="btn btn-sm btn-dark" target="_blank" rel="noopener noreferrer">Read More</a>
           </div>
       </div>
     )
