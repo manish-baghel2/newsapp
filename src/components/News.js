b95b922c9a6ec963a39e0f84aace64097662030a
Manish Baghel <manish_11914130@nitkkr.ac.in>
2022-09-22 19:30:50 +0530

    updated api key and refractioned some code

--- src/components/News.js
+++ src/components/News.js
@@ -6,16 +6,16 @@ import PropTypes from 'prop-types'
 export class News extends Component {
 
   static defaultProps = {
-    country : "in",
-    pageSize : 8,
-    category : 'general',
+    country: "in",
+    pageSize: 8,
+    category: 'general',
   }
 
   static propTypes = {
-    country : PropTypes.string,
-    pageSize : PropTypes.number,
-    category : PropTypes.string,
-    
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string,
+
   }
 
   constructor() {
@@ -26,59 +26,41 @@ export class News extends Component {
       page: 1
     }
   }
-  async componentDidMount() {
-    // console.log("cdm");
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=0cb0994a6753490699a14dca55b1fd35&page=1&pageSize=${this.props.pageSize}`;
-    this.setState({loading:true});
-    let data = await fetch(url);
-    let parsedata = await data.json();
-    console.log(parsedata);
-    this.setState({
-      articles: parsedata.articles,
-      totalResults: parsedata.totalResults,
-      loading:false
-    })
+
+  componentDidMount() {
+    this.updateNews();
   }
 
-  handlePreviousClick = async () => {
-    console.log("previous");
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=0cb0994a6753490699a14dca55b1fd35&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
-    this.setState({loading:true});
+  async updateNews() {
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=3698dc196cd54a5594c4e66869be4b4f&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
     let data = await fetch(url);
     let parsedata = await data.json();
-    console.log(parsedata);
     this.setState({
-      page: this.state.page - 1,
       articles: parsedata.articles,
-      loading:false
+      totalResults : parsedata.totalResults,
+      loading: false
     })
+  }
+  handlePreviousClick = async () => {
+    this.setState({ page: this.state.page - 1 })
+    this.updateNews();
   }
 
   handleNextClick = async () => {
-    console.log("Next");
-    if (!(this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize))) {
-      let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=0cb0994a6753490699a14dca55b1fd35&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
-    this.setState({loading:true});
-      let data = await fetch(url);
-      let parsedata = await data.json();
-      console.log(parsedata);
-      this.setState({
-        page: this.state.page + 1,
-        articles: parsedata.articles,
-        loading:false
-      })
-    }
+    this.setState({ page: this.state.page + 1 });
+    this.updateNews();
   }
 
   render() {
     return (
       <div className="container my-3">
-        <h1 className="text-center" style={{margin : '35px'}}>
+        <h1 className="text-center" style={{ margin: '35px' }}>
           NewsMonkey - {this.props.category.toUpperCase()}
         </h1>
-        {this.state.loading&&<Spinner/>}
+        {this.state.loading && <Spinner />}
         <div className="row">
-          {this.state.articles.map((elements) => {
+          {this.state.articles && this.state.articles.map((elements) => {
             return <div className="col-md-3 my-2" key={elements.url}>
               <NewsItem title={elements.title ? elements.title.slice(0, 45) : ""} description={elements.description ? elements.description.slice(0, 80) : ""} imageUrl={elements.urlToImage} newsUrl={elements.url} author={elements.author} date={elements.publishedAt} source={elements.source.name} category={this.props.category}></NewsItem>
             </div>
